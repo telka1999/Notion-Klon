b6ca18849ffce9319254f85d904a6eed8f29574e
bartosz <bartosz@bartosz1999.local>
2022-05-04 10:56:07 +0200

    Final project

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
 // Sroll bottom border
 
-
-function navscroll (){
-    const bodyElement = document.querySelector('body');
-    const navElement = document.querySelector('.header-main');
-    this.scrollY > 100 ? navElement.classList.add('website-header-border'): navElement.classList.remove('website-header-border');
+function navscroll() {
+  const bodyElement = document.querySelector("body");
+  const navElement = document.querySelector(".header-main");
+  this.scrollY > 100
+    ? navElement.classList.add("website-header-border")
+    : navElement.classList.remove("website-header-border");
 }
 
-window.addEventListener('scroll', navscroll, false);
-
+window.addEventListener("scroll", navscroll, false);
 
 // Humburger toggle
 
+const navToggle = document.querySelector(".humburger-icon");
 
-const navToggle = document.querySelector('.humburger-icon');
-
-navToggle.addEventListener('click', ()=> {
-    document.body.classList.toggle('nav-open')
-    document.body.classList.toggle('icon-hide')
-})
-
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+  document.body.classList.toggle("icon-hide");
+});
 
 // Mobile menu tabs
 
+const tabs = document.querySelectorAll("[data-id]");
+const icon = document.querySelectorAll(".mobile-btn-arrow");
 
-const tabs = document.querySelectorAll('[data-id]');
-const icon = document.querySelectorAll('.mobile-btn-arrow');
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.id);
+    target.classList.toggle("menu-mobile-tab-active");
+  });
+});
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.id)
-        target.classList.toggle('menu-mobile-tab-active')
-
-    })
-})
-
-icon.forEach(icon => {
-    icon.addEventListener('click', ()=>{
-        icon.classList.toggle('mobile-btn-arrow-two')
-    })
-})
-
+icon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    icon.classList.toggle("mobile-btn-arrow-two");
+  });
+});
 
 // Team carusel
-
 
 const btns = document.querySelectorAll('[data-cllas]');
 const caruselimg = document.querySelectorAll('[data-carusel-img-hide]');
 
+function clearImgBtn(){
+  caruselimg.forEach(caruselimg => {
+      caruselimg.classList.remove('team-carusel-img-show')
+  })
+  btns.forEach(btn => {
+      btn.classList.remove('btn-active')
+  })
+}
+
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
+      clearInterval(autoCarusel)
         const targetimg = document.querySelector(btn.dataset.cllas)
-        caruselimg.forEach(caruselimg => {
-            caruselimg.classList.remove('team-carusel-img-show')
-        })
-        btns.forEach(btn => {
-            btn.classList.remove('btn-active')
-        })
+        clearImgBtn()
         targetimg.classList.add('team-carusel-img-show') 
         btn.classList.add('btn-active')
     })
@@ -71,47 +70,65 @@ btnslogos.forEach(btnlogo => {
     })
 })
 
+//Auto carusel
+
+const logoImg = document.querySelectorAll(".logo-img");
+const bigImg  = document.querySelectorAll(".big-img");
+let position = 0;
+
+const autoCarusel = setInterval(()=>{
+
+  clearImgBtn()
+
+  position +=1
+
+  if(position === 6){
+    position = 0;
+  }
+
+  bigImg[position].classList.add('team-carusel-img-show');
+  btns[position].classList.add('btn-active');
+  logoImg[position].classList.add('team-carusel-img-show');
+
+},4000)
 
 // Template Carusel
 
-
 let slidePosition = 0;
-const slides = document.getElementsByClassName('templates-carusel');
+const slides = document.getElementsByClassName("templates-carusel");
 const totalSlide = slides.length;
 
-document.getElementById('btn-next').addEventListener('click',()=> {
-    moveToNextSlide();
+document.getElementById("btn-next").addEventListener("click", () => {
+  moveToNextSlide();
 });
 
-document.getElementById('btn-prev').addEventListener('click', ()=> {
-    moveToPrevSlide();
+document.getElementById("btn-prev").addEventListener("click", () => {
+  moveToPrevSlide();
 });
 
 function updateSlidePosition() {
-    for(let slide of slides) {
-        slide.classList.remove('templates-carusel-show');
-        slide.classList.add('templates-carusel-hide');
-    }
-    slides[slidePosition].classList.remove('templates-carusel-hide')
-    slides[slidePosition].classList.add('templates-carusel-show');
+  for (let slide of slides) {
+    slide.classList.remove("templates-carusel-show");
+    slide.classList.add("templates-carusel-hide");
+  }
+  slides[slidePosition].classList.remove("templates-carusel-hide");
+  slides[slidePosition].classList.add("templates-carusel-show");
 }
 
 function moveToNextSlide() {
-
-    if(slidePosition === totalSlide - 1){
-        slidePosition = 0;
-    }else{
-        slidePosition++;
-    }
-    updateSlidePosition();
+  if (slidePosition === totalSlide - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
 }
 
 function moveToPrevSlide() {
-
-    if(slidePosition === 0){
-        slidePosition = totalSlide -1;
-    }else{
-        slidePosition--;
-    }
-    updateSlidePosition();
+  if (slidePosition === 0) {
+    slidePosition = totalSlide - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
 }
